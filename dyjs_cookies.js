47ced2f0e50fa8355cd8566f835e530ddb5bc320
lixing5671ac <86886762+lixing5671ac@users.noreply.github.com>
2022-03-07 15:58:00 +0800

    Update dyjs_cookies.js

--- dyjs_cookies.js
+++ dyjs_cookies.js
@@ -3,7 +3,7 @@ let cookies = [
     {
         "name": "账号1",
         "Cookie": "passport_csrf_token=bb9b51c994caf0383b28e8a85e803f62; passport_csrf_token_default=bb9b51c994caf0383b28e8a85e803f62; d_ticket=4ad655babe18f182927996df619c9f4af489b; n_mh=jJi_74ocdL0Rz2_gYsk1UmYjl0X-1uU2AnEDa_duryo; odin_tt=025f86f4d724d246bffb1dbf282399095da12fe9c174d74840bf64686822da7ef934ad23a94d6710e0c6e02a03c1b39dca5c1034a7dd185335bcef7add8788f67905e02e2998a972769af17f15a0e83d; sessionid=c1bfc2da8590b50dd599d0b30872e2e6; sessionid_ss=c1bfc2da8590b50dd599d0b30872e2e6; sid_guard=c1bfc2da8590b50dd599d0b30872e2e6|1646061143|5184000|Fri,+29-Apr-2022+15:12:23+GMT; sid_tt=c1bfc2da8590b50dd599d0b30872e2e6; uid_tt=2135ea93a9e9a0c2ee610b92ad702011; uid_tt_ss=2135ea93a9e9a0c2ee610b92ad702011; MONITOR_WEB_ID=299486371131038; install_id=75183338041040; ttreq=1$c9ec662cc1e6e7e91acf9b0cefa24db659c9c244",
-        "x-Tt-Token": "zzzzzzzzzzzzzzzzzzz"
+        "x-Tt-Token": "00c1bfc2da8590b50dd599d0b30872e2e602c9c0220b0867beb40f678d962d04ad9a43892708daec8a2c3d42b1fb6d53e44d281d68fd9f3d5eb7e1e7ed5e73f5c8f737e2ff2536cfdb819a12eeb139b37b9cf7a5f527a2765070e27fd38574aaae0b9-1.0.1"
     },
     {
         "name": "账号2",
