bbe353d7956e6c6458aff16c949fb862f33ec8e7
lixing5671ac <86886762+lixing5671ac@users.noreply.github.com>
2022-06-06 10:21:45 +0800

    Add files via upload

---
+++ soy_variable_data.js
@@ -0,0 +1,11 @@
+//添加账号后需要在后面添加一个逗号(英文输入法的),
+//最后一个账号后面是没有逗号的
+//变量不能使用单引号引起
+module.exports = {"code":200,
+"variable_data":{
+"kyin":[//这里是脚本标识,下面的 kyin 就是给快音脚本使用的标识
+"31E60149-6C36-4326-B136-4F5B23A6EF7A& eyJleHAiOjE2NTQ0NzY0MzQsImtpZCI6NiwiYWxnIjoiSFMyNTYifQ.eyJ1aWQiOiIxMDQzNjI2NTMiLCJvcGVuaWQiOiJvWWJWTHhBWmVQcVJBTlNBUzBfbzFWUVpuUXl3IiwiZXhwIjoxNjU0NDc2NDM0fQ.3jCHUqiQtcNKBZCWN1Clk6w7mEGqjTKDxxGeF-lB0_A& eyJleHAiOjE2NjIyNTA2MzQsImFsZyI6IkhTMjU2In0.eyJ1aWQiOiIxMDQzNjI2NTMiLCJvcGVuaWQiOiJvWWJWTHhBWmVQcVJBTlNBUzBfbzFWUVpuUXl3IiwiZXhwIjoxNjYyMjUwNjM0fQ.Xt5nP2wYX_4mM2er54zgO8emZ96b-x9a171z2O_GMxQ",//这是第一个账号
+"123456",//这是第二个账号
+"123456"//这是第三个账号
+]}
+}
